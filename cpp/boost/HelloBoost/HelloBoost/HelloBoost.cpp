bd0606b5b1952b753542c90a1d1f05f119246e4a
ben1222 <ben_281493815@qq.com>
2016-02-14 16:26:50 +0800

    Try c++ boost
    Followed tutorial and tried boost::lambda

---
+++ cpp/boost/HelloBoost/HelloBoost/HelloBoost.cpp
@@ -0,0 +1,30 @@
+// HelloBoost.cpp : Defines the entry point for the console application.
+//
+
+// Following boost guide: Getting Started on Windows
+// http://www.boost.org/doc/libs/1_60_0/more/getting_started/windows.html
+
+#include "stdafx.h"
+
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int hello_boost_lambda()
+{
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << '\n');
+
+	return 0;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	hello_boost_lambda();
+	return 0;
+}
+
